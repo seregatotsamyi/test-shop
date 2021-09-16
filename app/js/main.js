5c5cd3d4cb077e604170a5994df1e12e50a91dc5
seregatotsamyi <iserg.mos.ls@gmail.com>
2021-09-16 15:29:54 +0500

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,12 +1,42 @@
-$(function(){
-  $('.header__slider__inner').slick({
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 3500,
-    draggable: false,
-    pauseOnFocus: false,
-    pauseOnHover: false,
+window.onload = function() {
+  var heart = document.getElementsByClassName("shop__img--heart");
+  for(var i = 0; i < heart.length; i++) {
+    var elem = heart[i];
+    elem.addEventListener("click", function(){
+        this.parentElement.classList.toggle("red");
+    });
+  }
+
+  var addToCart = document.getElementsByClassName("shop__item__btn");
+  for(var i = 0; i < addToCart.length; i++) {
+    var elem1 = addToCart[i];
+    elem1.addEventListener("click", function(e){
+        e.preventDefault();
+        this.parentElement.parentElement.classList.toggle("active-count");
+    });
+  }
+
+  const btns = document.querySelectorAll('.counter__btn');
+  btns.forEach(btn=>{
+    btn.addEventListener('click', function(){
+      const direction = this.dataset.direction;
+      const inp = this.parentElement.querySelector('.counter__input');
+      const currentValue = +inp.value;
+      let newValue;
+
+      if (direction === 'plus') {
+        newValue = currentValue + 1;
+      } else {
+        newValue = currentValue - 1 > 1 ? currentValue - 1 : 1;
+        if (currentValue === 1){
+          this.parentElement.parentElement.classList.toggle("active-count");
+        }
+      }
+      inp.value = newValue;
+    })
+  
   });
-  $(".input-tel").mask("+9 (999) 99-99-999");
-});
+ 
+
+};
+  
